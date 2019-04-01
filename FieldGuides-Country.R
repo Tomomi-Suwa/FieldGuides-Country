152a1e777bae468025ab9f4917622c6a5b8fd468
Tomomi Suwa <tsuwa@ACTN-TSuwa-Mac-Book-Pro.local>
2019-04-01 11:58:15 -0500

    update

--- FieldGuides-Country.R
+++ FieldGuides-Country.R
@@ -7,14 +7,76 @@ library(shiny)
 
 
 #UI----
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+ui <- fluidPage(
+  titlePanel("Uploading Files"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('file1', 'Choose file to upload',
+                accept = c(
+                  'text/csv',
+                  'text/comma-separated-values',
+                  'text/tab-separated-values',
+                  'text/plain',
+                  '.csv',
+                  '.tsv'
+                )
+      ),
+      tags$hr(),
+      #select box
+      selectInput("country_input", "Select a Country", choices = unique(file1$Category)),
+      checkboxInput('header', 'Header', TRUE),
+      radioButtons('sep', 'Separator',
+                   c(Comma=',',
+                     Semicolon=';',
+                     Tab='\t'),
+                   ','),
+      tags$hr(),
+      p('Please upload files downloaded from Drupal'
+      )
+    ),
+    mainPanel(
+      #to show the contents of the imported file 
+      #tableOutput('contents')
+      
+      #to show percent summary table
+      tableOutput('summary')
+    )
+  )
 )
 
-#Server#####
-server <- function(input, output) { }
+# By default, the file size limit is 5MB. It can be changed by
+# setting this opztion. Here we'll raise limit to 9MB.
+  options(shiny.maxRequestSize = 9*1024^2)
+  
+#Server----
+server<-function(input, output, session) {
 
+  #Reactive value for selected dataset
+  dataInput<-reactive({
+    get(input$dataInput)
+  })
+  observe({
+    updateSelectInput(session, "country_input", choices = unique(dataInput()$country))
+    
+  })
+  #to create a summary table
+  output$summary_table <-renderTable ({
+    summary<-dataInput()
+    if(is.null(summary))
+      return(Null)
+  })
+  
+}
+  
+#To createa table of the file content
+# output$contents <- renderTable({
+#   inFile <- input$file1
+# 
+#   if (is.null(inFile))
+#     return(NULL)
+#   
+#   read.csv(inFile$datapath, header = input$header,
+#            sep = input$sep)
+# })
 #Run Shiny app
 shinyApp(ui, server)
---
+++ app.R
@@ -0,0 +1,81 @@
+library(shiny)
+
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('datafile', 'Choose CSV file',
+                accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+      conditionalPanel(
+        # use a server side condition
+        condition = "output.fileUploaded",
+        # placeholders will be replaced from the server
+        selectInput("country_input", "Select a cunttry", "placeholder 1")
+        #selectInput("level", "select level", "placeholder 2")
+      )
+    ),
+    mainPanel(
+      h3("Field Guides by Country"),
+      plotOutput("PieChart"),
+      tableOutput("summary_table"),
+      tableOutput("table")
+    )
+  )
+)
+
+server <- function(input, output, session){
+  # create reactive version of the dataset (a data.frame object)
+  filedata <- reactive({
+    infile <- input$datafile
+    if (is.null(infile))
+      # User has not uploaded a file yet. Use NULL to prevent observeEvent from triggering
+      return(NULL)
+    temp <- read.csv(infile$datapath)
+    temp[order(temp[, 1]),]
+  })
+  
+  # dataInput<-reactive({
+  #   dataInput2<- filedata() %>%
+  #     filter(country %in% input$country_input) %>%
+  #     select(country, Category) %>%
+  #     group_by(Category) %>%
+  #     summarise(count=n()) %>%
+  #     mutate(Percent = count/sum(count)) %>%
+  #     as.data.frame()
+  #   if (is.null(filedata))
+  #     # User has not uploaded a file yet. Use NULL to prevent observeEvent from triggering
+  #     return(NULL)
+  # })
+  #print(dataInput())
+
+  # inform conditionalPanel wheter dropdowns sohould be hidden
+  output$fileUploaded <- reactive({
+    return(!is.null(filedata()))
+  })
+  outputOptions(output, 'fileUploaded', suspendWhenHidden=FALSE)
+  
+  # update the selectInput elements according to the dataset
+  
+  ## update 'country_input' selector
+  observeEvent(filedata(), {
+    updateSelectInput(session, "country_input", choices = unique(filedata()$country))
+  })
+  
+  #show a pie chart 
+  # output$PieChart<-renderPlot({
+  #   dataInput() %>% 
+  #     ggplot(aes(x="", y=Percent, fill=Category))+geom_bar(width = 1, stat = "identity")+ coord_polar("y", start=0)
+  # },bordered = TRUE)
+  
+  # show summary table (percentge)
+#   output$summary_table <- renderTable({
+#     filedata()%>% subset( filedata()$country == input$country_input)
+#   }, bordered = TRUE)
+# }
+  # show table
+  output$table <- renderTable({
+    subset(filedata(), filedata()$country == input$country_input)
+  }, bordered = TRUE)
+}
+ 
+
+shinyApp(ui, server)
